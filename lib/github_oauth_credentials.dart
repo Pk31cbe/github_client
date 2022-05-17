c14dd4133f2ef155bfb1aa1ced51ed602d98f3fb
Pk31 <praveenkumar.selvaraj.31@gmail.com>
2022-05-17 19:15:02 +0530

    change of code

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '10e69a5ba9be19124fd5';
+const githubClientSecret = '10e69edb69b1cdd87caa44f8ae59af9fed279e9b';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
